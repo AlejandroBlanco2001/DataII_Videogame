2101344d664f1263f674ac44d561ef11747b7352
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2020-11-13 18:49:02 -0500

    Fixed Import in index.js for Phaser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Phaser from "phaser";
+import Phaser from "../node_modules/phaser";
 import MainMenu from "./scenes/MainMenu";
 import Lobby from "./scenes/Lobby";
 import BallGame from "./scenes/BallGame";
