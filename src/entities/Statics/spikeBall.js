e6fa246d8422cb5c3aa76efb4f68546c1dfddec8
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2020-11-19 14:03:05 -0500

    Bouncing Ball and Kill Player

--- src/entities/Statics/spikeBall.js
+++ src/entities/Statics/spikeBall.js
@@ -1,3 +1,26 @@
-export default class spikeBall extends Phaser.GameObjects{
-    
+export default class spikeBall extends Phaser.Physics.Arcade.Sprite{
+    constructor(scene,x,y,texture){
+        super(scene,x,y,texture);
+        scene.sys.updateList.add(this);
+        scene.sys.displayList.add(this);
+        scene.physics.world.enableBody(this);
+
+        this.body.velocity.setTo(200,200);
+
+        // AABB and Size
+        this.setCollideWorldBounds(true); 
+        
+        this.setScale(0.2);
+        this.body.setCircle(190);
+
+        this.setAngularVelocity(54);
+        this.body.bounce.set(1);
+    }
+
+    faster(){
+        if(this.angularV <= 1000){
+            this.angularV += 50;
+            this.setAngularVelocity(this.angularV);
+        }
+    }
 }
--- src/scenes/BallGame.js
+++ src/scenes/BallGame.js
@@ -1,4 +1,5 @@
 import Player from "../entities/Character/Player";
+import SpikeBall from "../entities/Statics/spikeBall";
 
 export default class BallGame extends Phaser.Scene{
     constructor(){
@@ -7,6 +8,7 @@ export default class BallGame extends Phaser.Scene{
             active: false
         });
         this.player;
+        this.spikeBall;
     }
 
     init(data){
@@ -27,6 +29,7 @@ export default class BallGame extends Phaser.Scene{
         });
         
         this.load.spritesheet("drake", "src/assets/images/SpriteSheets/blue.png",{framHeight: 24, frameWidth: 24});
+        this.load.image("spike","src/assets/images/Statics/spikeBall.png");
 
         this.load.image("terrain", "src/assets/images/sci-fi-tileset.png");
         this.load.tilemapTiledJSON("spikeBallMap","src/assets/map/spikeBallMap.json");
@@ -36,14 +39,16 @@ export default class BallGame extends Phaser.Scene{
         this.player = new Player(400,400,this,"drake",this.username);
         let spikeBallMap = this.add.tilemap("spikeBallMap");
         this.terrain = spikeBallMap.addTilesetImage("sci-fi-tileset","terrain");
-        
+        this.spikeBall = new SpikeBall(this,400,400,"spike");
 
         this.keyboard = this.input.keyboard.addKeys("W, A, S, D");
+
         // layers 
         let topLayer = spikeBallMap.createStaticLayer("bot", [this.terrain], 0, 0).setDepth(-1);
         let botLayer = spikeBallMap.createStaticLayer("top", [this.terrain], 0, 0);
-   
-        //map collisions
+        let spikeBallLayer = spikeBallMap.createStaticLayer("BoxPattern",[this.terrain], 0, 0).setDepth(-2);
+        
+        //map collisions with Player and SpikeBall
         this.physics.add.collider(this.player, botLayer);
         botLayer.setCollisionByProperty({
             collides: true
@@ -54,19 +59,35 @@ export default class BallGame extends Phaser.Scene{
             collides: true
         });
 
+        this.physics.add.collider(this.spikeBall,spikeBallLayer, () =>{
+            this.spikeBall.faster();
+        });
+
+        spikeBallLayer.setCollisionByProperty({
+            hitByBall: true
+        })
+
+        // collisions with player and spikeBall
+        this.physics.add.collider(this.player,this.spikeBall, () =>{
+            this.player.destroy();
+        });s
+
+        /*
         const debugGraphics = this.add.graphics().setAlpha(0.7);
 
-        /* DEBUG GRAPHICS
         topLayer.renderDebug(debugGraphics,{
             tileCOlor: null,
             collidingTileColor: new Phaser.Display.Color(243, 234,48,255),
             faceColor: new Phaser.Display.Color(43,39,37,255)
         });
         */
+       
     }
 
     update(time, delta){
-        this.player.update(this.keyboard);
+        if(this.player.active){
+            this.player.update(this.keyboard);
+        }
     }
 
 
